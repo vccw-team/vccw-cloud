d1800bf4a370308e02dab8559be94138a9259a39
miya0001 <miya@wpist.me>
2017-08-12 15:56:19 +0900

    change repo url

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 set -ex
 
 PLAYBOOK_DIR=$HOME/.playbook
-PLAYBOOK_REPO=https://github.com/coderdojo-japan/dojopaas-wordpress.git
+PLAYBOOK_REPO=https://github.com/vccw-team/vccw-cloud
 
 ansible localhost -m git -a "repo=${PLAYBOOK_REPO} dest=${PLAYBOOK_DIR} version=HEAD"
 cd $PLAYBOOK_DIR
