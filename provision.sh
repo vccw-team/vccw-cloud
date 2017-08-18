5c012ce6fe8629307646f2a56b5e3af07222aa51
miya0001 <miya@wpist.me>
2017-08-18 13:45:05 +0900

    remove hosts and allow override it

--- provision.sh
+++ provision.sh
@@ -2,9 +2,18 @@
 
 set -ex
 
+sudo apt install ansible
+
 PLAYBOOK_DIR=$HOME/.playbook
 PLAYBOOK_REPO=https://github.com/vccw-team/vccw-cloud
 
 ansible localhost -m git -a "repo=${PLAYBOOK_REPO} dest=${PLAYBOOK_DIR} version=HEAD"
 cd $PLAYBOOK_DIR
+
+if [ ! -f hosts ]; then
+  cat << EOS > hosts
+hosts=127.0.0.1
+EOS
+fi
+
 ansible-playbook provision/playbook.yml --connection=local
